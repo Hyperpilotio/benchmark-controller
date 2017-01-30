1eb5739a71048c3aff70a3bfdd1088ca73419ea8
swh <swh@hsiang.io>
2017-01-31 04:19:04 +0800

    * Support mysql benchmarking.

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ echo "Build base image with Open JDK 8"
 
 docker build -f Dockerfile_openjdk_8 -t wen777/bench:base-openjdk-8 .
 
+echo "Build base image with mysql-tpcc"
+
+docker build -f Dockerfile_mysql_tpcc -t wen777/bench:base-mysql-tpcc .
+
 echo
 echo "Push images to docker hub"
 echo
@@ -16,4 +20,4 @@ docker push wen777/bench:base
 
 docker push wen777/bench:base-openjdk-8
 
-
+docker push wen777/bench:base-mysql-tpcc
