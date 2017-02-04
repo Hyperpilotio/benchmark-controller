f3d8dfa99fee0d04e792ed5f4b4074a46b0efea8
swh <swh@hsiang.io>
2017-02-04 13:41:21 +0800

    move images to hyperpilot's repo

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@
 
 echo "Build base image"
 
-docker build -t wen777/bench:base .
+docker build -t hyperpilot/bench:base .
 
 echo "Build base image with Open JDK 8"
 
-docker build -f Dockerfile_openjdk_8 -t wen777/bench:base-openjdk-8 .
+docker build -f Dockerfile_openjdk_8 -t hyperpilot/bench:base-openjdk-8 .
 
 echo "Build base image with mysql-tpcc"
 
-docker build -f Dockerfile_mysql_tpcc -t wen777/bench:base-mysql-tpcc .
+docker build -f Dockerfile_mysql_tpcc -t hyperpilot/bench:base-mysql-tpcc .
 
 echo
 echo "Push images to docker hub"
 echo
 
-docker push wen777/bench:base
+docker push hyperpilot/bench:base
 
-docker push wen777/bench:base-openjdk-8
+docker push hyperpilot/bench:base-openjdk-8
 
-docker push wen777/bench:base-mysql-tpcc
+docker push hyperpilot/bench:base-mysql-tpcc
