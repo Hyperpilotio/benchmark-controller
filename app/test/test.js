5fb9d90039b136be15dae45658fb708dfb277532
swh <swh@hsiang.io>
2017-05-26 04:06:58 +0800

    Add tests for issue #10

--- app/test/test.js
+++ app/test/test.js
@@ -1,8 +1,30 @@
-var assert = require('assert');
+const assert = require('assert');
+const server = require('../bin/benchmarks-ui').app;
+const request = require('request');
 
 describe('Start server', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+  let instance = {};
+  before(function() {
+    instance = server();
+  });
+  after(function() {
+    instance.close();
+  });
+  describe('#post.("/")', function() {
+    it('should return error message in JSON format when error happens', function(done) {
+      request.post('http://localhost:6001', {}, function(err, res, body) {
+        assert.equal(404, res.statusCode);
+        done();
+      });
+    });
+  });
+
+  describe('#post.("/api/benchmarks")', function() {
+    it('should return error message in JSON format when error happens', function(done) {
+      request.post('http://localhost:6001/api/benchmarks', {}, function(err, res, body) {
+        assert.equal(500, res.statusCode);
+        done();
+      });
     });
   });
 });
