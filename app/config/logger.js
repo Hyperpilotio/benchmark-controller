a97dc7f42087920409664b626fb3b60b5bf85da2
Timothy Chen <tnachen@gmail.com>
2017-07-08 23:15:04 +0000

    Log verbose to file in container

--- app/config/logger.js
+++ app/config/logger.js
@@ -4,9 +4,10 @@ const logger = new (winston.Logger)({
     transports: [
 
       new (winston.transports.Console)({'timestamp':true, 'colorize': true}),
-        new (winston.transports.File)({
-            'filename': '/tmp/benchmark-controller.log'
-        })
+      new (winston.transports.File)({
+          'filename': '/tmp/benchmark-controller-verbose.log',
+          'level': 'verbose'
+      })
     ]
 });
 
