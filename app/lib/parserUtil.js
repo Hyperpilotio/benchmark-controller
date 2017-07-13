afc19cecd192653ed29d36409c4c1ea1bab855e1
swhsiang <swh@hsiang.io>
2017-07-13 14:48:05 -0700

    Implement parserUtil feature to create a parser for each request

---
+++ app/lib/parserUtil.js
@@ -0,0 +1,59 @@
+/**
+ * This parserManager.js aims at provide useful functions to manage parsers for multiple workloads
+ */
+
+const config = require('../config/config');
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+
+const generateParserPath = function (stageID) {
+    const dest = config.parserStoragePath ? config.parserStoragePath : '/tmp';
+    return path.join(dest, `${stageID}.js`);
+}
+
+const downloadParserAsync = function (dest, url) {
+    return new Promise(function (resolve, reject) {
+        // Make sure we always download latest parser
+        fs.unlink(dest);
+
+        const req = request.get(url);
+        // verify response code
+        req.on('response', function (response) {
+            if (response.statusCode !== 200) {
+                reject(new Error(`Response status was ${response.statusCode}`));
+            }
+        });
+
+        // check for request errors
+        req.on('error', function (err) {
+            fs.unlink(dest);
+            reject(new Error(err.message));
+        });
+
+        const file = fs.createWriteStream(dest);
+        file.on('finish', function () {
+            // close() is async, call cb after close completes.
+            resolve(file.close());
+        });
+
+        file.on('error', function (err) {
+            // Delete the file async. (But we don't check the result)
+            fs.unlink(dest);
+            reject(err.message);
+        });
+
+        req.pipe(file);
+    });
+}
+
+const CreateParserAsync = async function(stageID, url) {
+    const dest = generateParserPath(stageID);
+    return downloadParserAsync(dest, url).then(()=>{
+        return require(dest);
+    });
+}
+
+module.exports = {
+    'CreateParserAsync': CreateParserAsync
+};
