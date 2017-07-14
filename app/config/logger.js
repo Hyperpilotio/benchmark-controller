a054d2b5b9153507a82110f2c71436132c2be592
swhsiang <swh@hsiang.io>
2017-07-14 12:08:55 -0700

    Use debug level for file logger

--- app/config/logger.js
+++ app/config/logger.js
@@ -1,12 +1,17 @@
 const winston = require('winston');
+const config = require('./config');
 
 const logger = new (winston.Logger)({
     transports: [
 
-      new (winston.transports.Console)({'timestamp':true, 'colorize': true}),
+      new (winston.transports.Console)({
+          'timestamp':true,
+          'colorize': 'all',
+          'level': config.logLevel ? config.logLevel : 'info'
+      }),
       new (winston.transports.File)({
           'filename': '/tmp/benchmark-controller-verbose.log',
-          'level': 'verbose'
+          'level': 'debug'
       })
     ]
 });
